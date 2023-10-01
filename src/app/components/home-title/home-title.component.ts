0755785629cec930bc0c2c43c3c7be3381134507
Márcio Vinícius <marcio.campos@outlook.com>
2023-10-01 18:29:58 -0300

    feat(home): add buttons

--- src/app/components/home-title/home-title.component.ts
+++ src/app/components/home-title/home-title.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-title',
   templateUrl: './home-title.component.html',
-  styleUrls: ['./home-title.component.scss']
+  styleUrls: ['./home-title.component.scss'],
 })
 export class HomeTitleComponent {
+  constructor(private router: Router) {}
 
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 }
