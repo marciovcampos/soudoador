b5e209be11e82fd1fd72fbb5ef7d7823fcc400a6
Márcio Vinícius <marcio.campos@outlook.com>
2023-10-01 13:54:23 -0300

    feat: add menu items

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { MenuItem } from 'src/app/shared/menu-item.model';
 
 const menuItems: MenuItem[] = [
+  { label: 'Campanhas', link: '/campanhas' },
+  { label: 'Onde doar', link: '/onde-doar' },
+  { label: 'FAQ', link: '/faq' },
+  { label: 'Cadastra-se', link: '/cadastro' },
   { label: 'Contato', link: '/contato' },
   { label: 'Sobre', link: '/sobre' },
 ];
