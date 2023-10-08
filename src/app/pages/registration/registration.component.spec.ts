90fd1ddf21954c44a13aacc9ee2531c87c4fd597
Márcio Vinícius <marcio.campos@outlook.com>
2023-10-08 17:23:20 -0300

    feat(registration): add fields in registration component

--- src/app/pages/registration/registration.component.spec.ts
+++ src/app/pages/registration/registration.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegistrationComponent } from './registration.component';
 import { PageTitleComponent } from 'src/app/components/page-title/page-title.component';
 import { TwoColumnLayoutComponent } from 'src/app/components/two-column-layout/two-column-layout.component';
@@ -15,7 +18,12 @@ describe('RegistrationComponent', () => {
         PageTitleComponent,
         TwoColumnLayoutComponent,
       ],
-      imports: [MatChipsModule],
+      imports: [
+        BrowserAnimationsModule,
+        MatChipsModule,
+        MatFormFieldModule,
+        MatInputModule,
+      ],
     });
     fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
