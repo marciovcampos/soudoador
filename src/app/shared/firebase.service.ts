144b73bf3c7dcef227aef7f96b3397b7ab28061f
Márcio Vinícius <marcio.campos@outlook.com>
2023-10-07 21:38:03 -0300

    refactor(firebase): refactor firebase service

--- src/app/shared/firebase.service.ts
+++ src/app/shared/firebase.service.ts
@@ -3,28 +3,42 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
-import { Instituion } from './instituion';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Instituion } from '../model/instituion';
+import { Campaign } from '../model/campaign';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
-  instituionsCollection: AngularFirestoreCollection<Instituion>;
+  private instituionsCollection: AngularFirestoreCollection<Instituion>;
+  private campaignCollection: AngularFirestoreCollection<Campaign>;
 
   constructor(private firestore: AngularFirestore) {
-    this.instituionsCollection = this.firestore.collection('instituions');
+    this.instituionsCollection =
+      this.firestore.collection<Instituion>('institutions');
+    this.campaignCollection = this.firestore.collection<Campaign>('campaigns');
   }
 
-  getAllInstituions(): Observable<Instituion[]> {
-    return this.instituionsCollection.snapshotChanges().pipe(
-      map((actions) =>
-        actions.map((a) => {
-          const data = a.payload.doc.data() as Instituion;
-          const id = a.payload.doc.id;
-          return { id, ...data };
-        })
-      )
-    );
+  getAllInstitutions(): Observable<Instituion[]> {
+    return this.instituionsCollection
+      .snapshotChanges()
+      .pipe(map((actions) => this.mapCollectionData<Instituion>(actions)));
+  }
+
+  getAllCampaigns(): Observable<Campaign[]> {
+    return this.campaignCollection
+      .snapshotChanges()
+      .pipe(map((actions) => this.mapCollectionData<Campaign>(actions)));
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private mapCollectionData<T>(actions: any[]): T[] {
+    return actions.map((a) => {
+      const data = a.payload.doc.data() as T;
+      const id = a.payload.doc.id;
+      return { id, ...data };
+    });
   }
 }
